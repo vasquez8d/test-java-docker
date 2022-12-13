d4a264cfc37afbf8fb09cf410c3f640403377e0b
Alexsandre Vasquez <Norman.Vasquez@somosoh.pe>
2022-12-13 13:14:02 -0500

    Refactor api users

--- src/main/java/com/alex/springboot/testdocker/app/controller/TestController.java
+++ src/main/java/com/alex/springboot/testdocker/app/controller/TestController.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alex.springboot.testdocker.app.models.Usuario;
+
+import reactor.core.publisher.Mono;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class TestController {
 
 	@GetMapping
-	public String hello() {
-		return "Hola Mundo!";
+	public Mono<Usuario> findAll() {
+		Usuario user = new Usuario("Alex", "Vasquez");
+		return Mono.just(user);
 	}
 	
 }
---
+++ src/main/java/com/alex/springboot/testdocker/app/models/Usuario.java
@@ -0,0 +1,15 @@
+package com.alex.springboot.testdocker.app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Usuario {
+	
+	private String id;
+	private String nombre;
+	
+}
